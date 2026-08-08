41bf4dfcdc00ee4de58ee63d83f041a7358183f6
jainarihant1309 <jainarihant1309@gmail.com>
2026-08-08 15:59:51 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -93,6 +93,3 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# Isko hata na hai 
-SECRET_AWS_KEY = "AKIAIOSFODNN7EXAMPLESecretKey" 
-
