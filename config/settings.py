d10cbd345ac4b19639d32cd18f6f8f8dd9b7e580
jainarihant1309 <jainarihant1309@gmail.com>
2026-08-08 15:37:38 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -93,3 +93,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Hardcoded secret key - SonarQube will flag this as a critical vulnerability
+SECRET_AWS_KEY = "AKIAIOSFODNN7EXAMPLESecretKey" 
+
